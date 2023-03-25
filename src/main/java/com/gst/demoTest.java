d843ccb40ffe9bcc374422146a5a14fe65dc9c12
atul9495 <atul@artcode.in>
2023-03-25 12:13:34 +0530

    update

--- src/main/java/com/gst/demoTest.java
+++ src/main/java/com/gst/demoTest.java
@@ -1,12 +1,11 @@
 package com.gst;
-import com.artcode.atul.*;
+ 
  
 public class demoTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-test test=new com.artcode.atul.test();
-System.out.println(test.add());
+ 
 	}
 
 }
